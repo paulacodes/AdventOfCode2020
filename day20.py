c416d56dc04967dd3f62fa386b6044f24000397c
paulacodes <pvd233@nyu.edu>
2020-12-20 13:33:18 +0100

    Remove print statements

--- day20.py
+++ day20.py
@@ -31,11 +31,6 @@ def find_matching_right_edges(tile, tiles):
 						if option[i][-1] == new_option[i][0]:
 							match += 1
 					if match == 10:
-						#print("RIGHT MATCH")
-						#print(tile)
-						#print("\n".join(map("".join, (option))))
-						#print(other_tile)
-						#print("\n".join(map("".join, (new_option))))
 						matching_edges.append([other_tile,option_index])
 	return(matching_edges)
 
@@ -87,30 +82,6 @@ def find_matching_bottom_edges(tile, tiles):
 						matching_edges.append([other_tile,option_index])
 	return(matching_edges)
 
-# def find_number_of_edges_with_matches(tile, tiles):
-# 	edges_with_matches = 0
-# 	for option in tiles[tile]:
-# 		for other_tile in tiles:
-# 			if other_tile != tile:
-# 				for new_option in tiles[other_tile]:
-# 					match = 0
-# 					for i in range(10):
-# 						if new_option[i][0] == option[i][-1]:
-# 							match += 1
-# 					if match == 10:
-# 						edges_with_matches += 1
-# 	for option in tiles[tile]:
-# 		for other_tile in tiles:
-# 			if other_tile != tile:
-# 				for new_option in tiles[other_tile]:
-# 					match = 0
-# 					for i in range(10):
-# 						if option[i][0] == new_option[i][-1]:
-# 							match += 1
-# 					if match == 10:
-# 						edges_with_matches += 1
-# 	return edges_with_matches
-
 def part1():
 	tiles = {}
 	data = []
@@ -127,58 +98,34 @@ def part1():
 	# Generate all possibile orientations of tiles
 	new_tiles = copy.deepcopy(tiles)
 	for tile in tiles:
-		#print("NEW TILE", str(tile))
-		#print("\n".join(map("".join, tiles[tile])))
 		new_tiles[tile] = [new_tiles[tile]]
-		# Flip vertically
-		#print("Flip vertically")
-		#print("\n".join(map("".join, flip_vertically(tiles[tile]))))
-		#print(new_tiles[tile])
 		new_list = flip_vertically(tiles[tile])
 		if new_list not in new_tiles[tile]:
 			new_tiles[tile].append(new_list)
-		#print(new_tiles[tile])
-		# Flip horizontally
-		#print("Flip horizontally")
-		#print("\n".join(map("".join, flip_horizontally(tiles[tile]))))
 		new_list = flip_horizontally(tiles[tile])
 		if new_list not in new_tiles[tile]:
 			new_tiles[tile].append(new_list)
 		rotated_list = copy.deepcopy(tiles[tile])
 		for i in range(3):
-			# Rotate once
-			#print("Rotation", str(i))
 			rotated_list = copy.deepcopy(list(zip(*reversed(rotated_list))))
 			if rotated_list not in new_tiles[tile]:
 				new_tiles[tile].append(rotated_list)
-			#print("\n".join(map("".join, rotated_list)))
-			# Flip vertically
-			#print("Flip vertically")
 			new_list = flip_vertically(rotated_list)
 			if new_list not in new_tiles[tile]:
 				new_tiles[tile].append(new_list)
-			#print("\n".join(map("".join, flip_vertically(rotated_list))))
-			# Flip horizontally
-			#print("Flip horizontally")
 			new_list = flip_horizontally(rotated_list)
 			if new_list not in new_tiles[tile]:
 				new_tiles[tile].append(new_list)
-			#print("\n".join(map("".join, flip_horizontally(rotated_list))))
-		print(len(new_tiles[tile]))
 
 	value_matches_dict = {}
+	matches_dict = {}
 	for tile in new_tiles:
-		print("Tile", str(tile))
 		matches = find_matching_right_edges(tile, new_tiles)
-		#print(matches)
 		matches = find_matching_left_edges(tile, new_tiles)
-		#print(matches)
 		matches = find_matching_top_edges(tile, new_tiles)
-		#print(matches)
 		matches = find_matching_bottom_edges(tile, new_tiles)
-		#print(matches)
-		print(len(matches))
 		value_matches_dict[tile] = len(matches)
+		matches_dict[tile] = matches
 
 	corners = nsmallest(4, value_matches_dict, key = value_matches_dict.get)
 	prod = 1
